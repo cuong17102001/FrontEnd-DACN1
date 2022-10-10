0071dffac338c14ecafbb9348a2410e16104fe65
nqcuong <nqcuong.1710@gmail.com>
2022-10-11 00:46:01 +0700

    11/10

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -6,26 +6,60 @@ import { UilPlayCircle } from "@iconscout/react-unicons"
 import { UilLocationPoint } from "@iconscout/react-unicons"
 import { UilSchedule } from "@iconscout/react-unicons"
 import { UilTimes } from "@iconscout/react-unicons"
+import { useDispatch, useSelector } from 'react-redux'
+import { uploadImage , uploadPost } from '../../actions/UploadAction'
 
 const PostShare = () => {
-
+    const loading = useSelector((state) => state.postReducer.uploading)
     const [image , setImage] = useState(null)
+    const {user} = useSelector((state) => state.authReducer.authData)
+    const desc = useRef()
     const imageRef = useRef()
+    const dispatch = useDispatch()
     const onChangeImage = (event)=>{
         if (event.target.files && event.target.files[0]) {
             let img = event.target.files[0]
-
-            setImage({
-                image : URL.createObjectURL(img)
-            })
+            setImage(img)
         }
     }
 
+    const reset = () =>{
+        setImage(null)
+        desc.current.value = ""
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+
+        const newPost = {
+            userId : user._id,
+            desc : desc.current.value
+        }
+
+        if (image) {
+            const data = new FormData()
+            const filename = Date.now() + image.name
+
+            data.append('name' , filename)
+            data.append('file' , image)
+            newPost.image = filename
+            try {
+                dispatch(uploadImage(data))
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        dispatch(uploadPost(newPost))
+        reset()
+    }
     return (
         <div className='PostShare'>
             <img src={ProfileImage} alt="" />
             <div>
-                <input type="text" placeholder='What is happening' />
+                <input
+                ref={desc}
+                required
+                type="text" placeholder='What is happening' />
                 <div className="postOptions">
                     <div className="option" style={{color: "var(--photo)"}}
                     onClick={()=>imageRef.current.click()}
@@ -41,8 +75,11 @@ const PostShare = () => {
                     <div className="option" style={{color: "var(--shedule)"}}>
                         <UilSchedule /> Shedule
                     </div>
-                    <button className='button ps-button'>
-                        Share
+                    <button className='button ps-button'
+                    onClick={handleSubmit}
+                    disabled={loading}
+                    >
+                        {loading ? "Loading" : "Share"}
                     </button>
                     <div style={{display:"none"}}>
                         <input type="file" accept='image/*' name='myFile' ref={imageRef} onChange={onChangeImage}/>
@@ -51,7 +88,7 @@ const PostShare = () => {
                 {image && (
                     <div className='previewImage'>
                         <UilTimes onClick={()=> setImage(null)}/>
-                        <img src={image.image} alt="" />
+                        <img src={URL.createObjectURL(image)} alt="" />
                     </div>
                 )}
             </div>
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,70 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Auth.css'
 import Logo from '../../img/logo.png'
+import { useDispatch, useSelector } from 'react-redux'
+import { logIn, signUp } from '../../actions/AuthAction'
 
 export const Auth = () => {
-  return (
-    <div className="Auth">
-        <div className="a-left">
-            <img src={Logo} alt="" />
-            <div className="Webname">
-                <h1>Media</h1>
-                <h6>Explore the ideas</h6>
+    const dispatch = useDispatch();
+    const loading = useSelector((state) => state.authReducer.loading)
+    const [isSignUp, setIsSignUp] = useState(true)
+
+    const [data, setData] = useState({
+        firstname: "",
+        lastname: "",
+        password: "",
+        confirmpassword: "",
+        username: ""
+    })
+    const [confirmPassword, setConfirmPassword] = useState(true)
+
+    const handleChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (isSignUp) {
+            data.password === data.confirmpassword ? dispatch(signUp(data)) : setConfirmPassword(false);
+        } else {
+            dispatch(logIn(data))
+        }
+    }
+
+    const resetForm = () => {
+        setConfirmPassword(true)
+        setData({
+            firstname: "",
+            lastname: "",
+            password: "",
+            confirmpassword: "",
+            username: ""
+        })
+    }
+    return (
+        <div className="Auth">
+            <div className="a-left">
+                <img src={Logo} alt="" />
+                <div className="Webname">
+                    <h1>Media</h1>
+                    <h6>Explore the ideas</h6>
+                </div>
             </div>
-        </div>
-        <Login/>
-    </div>
-  )
-}
 
-function Login(){
-    return(
-        <div className="a-right">
-            <form className='infoForm authForm' action="">
-                <h3>Login</h3>
-                <div>
-                    <input type="text" placeholder='User Name' 
-                    className='infoInput' name='username'/>
-                </div>
-                <div>
-                    <input type="text" placeholder='Password' 
-                    className='infoInput' name='password'/>
-                </div>
-                <div>
-                    <span style={{fontSize:"12px"}}>Don't have account? SignUp</span>
-                </div>
-                <button type='submit' className='button infoButton'>Login</button>
-            </form>
-        </div>
-    )
-}
+            <div className="a-right">
+                <form className='infoForm authForm' onSubmit={handleSubmit}>
+                    <h3>{isSignUp ? "Sign Up" : "Log In"}</h3>
 
-function SignUp(){
-    return(
-        <div className="a-right">
-            <form className='infoForm authForm' action="">
-                <h3>Sign Up</h3>
-                <div>
-                    <input type="text" placeholder='First Name' 
-                    className='infoInput' name='firstname'/>
-                    <input type="text" placeholder='Last Name' 
-                    className='infoInput' name='lastname'/>
-                </div>
-                <div>
-                    <input type="text" placeholder='User Name' 
-                    className='infoInput' name='username'/>
-                </div>
-                <div>
-                    <input type="text" placeholder='Password' 
-                    className='infoInput' name='password'/>
-                    <input type="text" placeholder='Confirm Password' 
-                    className='infoInput' name='confirmpassword'/>
-                </div>
-                <div>
-                    <span style={{fontSize:"12px"}}>Already have account? LOGIN</span>
-                </div>
-                <button type='submit' className='button infoButton'>Sign Up</button>
-            </form>
+                    {isSignUp &&
+                        <div>
+                            <input type="text" placeholder='First Name'
+                                className='infoInput' name='firstname'
+                                onChange={handleChange}
+                                value={data.firstname} />
+                            <input type="text" placeholder='Last Name'
+                                className='infoInput' name='lastname'
+                                onChange={handleChange}
+                                value={data.lastname} />
+                        </div>
+                    }
+
+                    <div>
+                        <input type="text" placeholder='User Name'
+                            className='infoInput' name='username'
+                            onChange={handleChange}
+                            value={data.username} />
+                    </div>
+                    <div>
+                        <input type="password" placeholder='Password'
+                            className='infoInput' name='password'
+                            onChange={handleChange}
+                            value={data.password} />
+
+                        {isSignUp &&
+                            <input type="password" placeholder='Confirm Password'
+                                className='infoInput' name='confirmpassword'
+                                onChange={handleChange}
+                                value={data.confirmpassword} />
+                        }
+
+                    </div>
+                    <span style={{ display: confirmPassword ? "none" : "block", color: "red", fontSize: "12px", alignSelf: "flex-end", marginRight: "5px" }}>
+                        * Confirm Password is not same
+                    </span>
+                    <div>
+                        <span style={{ fontSize: "12px", cursor: "pointer" }} onClick={() => { setIsSignUp((prev) => !prev); resetForm() }}>
+                            {isSignUp ? "Already have account? LOGIN!" : "Don't have account? SignUp!"}
+                        </span>
+                    </div>
+                    <button type='submit' className='button infoButton'>{loading ? "Loading..." : isSignUp ? "Sign Up" : "Log In"}</button>
+                </form>
+            </div>
+
         </div>
     )
 }
